30a4adc1a44f470c435c8b4ba7cdd9d8c98f8ad8
Boa Matule <boa.matule@gmail.com>
2019-07-02 06:35:41 +0200

    Manual testing with redis-cli...

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,16 +1,44 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
-// vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file. JavaScript code in this file should be added after the last require_* statement.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
-//= require rails-ujs
-//= require activestorage
-//= require turbolinks
-//= require_tree .
+// web_notification_channel.js
+App.notifications = App.cable.subscriptions.create({
+  channel: "WebNotificationsChannel"
+}, {
+      container() {
+          const container = document.getElementById('notifications');
+          return container;
+      },
+
+      connected() {
+          // Called when the subscription is ready for use on the server
+          // Display the active connection message (for demo purposes only)
+          this.container().innerHTML = '<p>Connected to Notification server</p>'
+          setTimeout(() => {
+              this.container().innerHTML = '';
+          }, 3000);  
+          console.log('Connected to websocket server ');
+      },
+
+      disconnected() {
+          // Called when the subscription has been terminated by the server
+          // Display the connection has been lost message (for demo purposes only)
+          this.container().innerHTML = '<p>Disconneced from server</p>'
+          setTimeout(() => {
+              this.container().innerHTML = '';
+          }, 3000);  
+          console.log('Disconneced');
+      },
+
+      received(data) {
+          // Create an element that will hold the message
+          let node = document.createElement('p');
+          node.innerHTML = data.message;
+          // Append the message to the element
+          this.container().appendChild(node)
+
+          setTimeout(() => {
+              // Remove the node after 3 seconds
+              this.container().removeChild(node);
+          }, 3000);
+      },
+
+  }
+);
